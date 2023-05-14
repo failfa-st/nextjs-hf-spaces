c78c16f4575a833bb81f8efebedbda9331be60bc
Tim Pietrusky <timpietrusky@gmail.com>
2023-05-14 23:32:27 +0200

    feat: added mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "@/lib/theme";
+import createEmotionCache from "@/lib/createEmotionCache";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <CssVarsProvider defaultMode="system" theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </CssVarsProvider>
+    </CacheProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,22 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
+import Container from "@mui/material/Container";
+import Footer from "@/components/footer";
+import Title from "@/components/title";
 
 export default function Home() {
   return (
     <>
-      <div className={styles.container}>
-        <Head>
-          <title>nextjs-docker-starter</title>
-          <link rel="icon" href="/favicon.ico" />
-          <meta name="description" content="Next.js in Docker on 🤗 Spaces" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
+      <Head>
+        <title>nextjs-docker-starter</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Next.js in Docker on 🤗 Spaces" />
+      </Head>
 
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            <a
-              href="https://nextjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Next.js
-            </a>{" "}
-            in{" "}
-            <a
-              href="https://www.docker.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Docker
-            </a>{" "}
-            on 🤗{" "}
-            <a
-              href="https://huggingface.co/spaces"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Spaces
-            </a>
-            !
-          </h1>
-        </main>
+      <Container component="main" sx={{ minHeight: "90vh" }}>
+        <Title />
+      </Container>
 
-        <footer className={styles.footer}>
-          <a href="https://failfa.st" target="_blank" rel="noopener noreferrer">
-            Powered by{" "}
-            <img
-              src="/failfast.svg"
-              alt="failfast Logo"
-              className={styles.logo}
-            />
-          </a>
-          <a
-            href="https://github.com/failfa-st/nextjs-docker-starter"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {" "}
-            Contribute on GitHub
-          </a>
-        </footer>
-      </div>
+      <Footer />
     </>
   );
 }
